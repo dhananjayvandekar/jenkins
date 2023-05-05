963a1cc65b398e235b87daa0bfb743949b9a0e72
dhanu <vandekardhananjay721@gmail.com>
2023-05-05 19:04:50 +0530

    dfgh

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage ('pull') {
             steps {
-                git 'git branch{ 'main'}, {url: 'https://github.com/dhananjayvandekar/All-task-and-doc.-of-AWS.git'}
+                git 'git branch:'main', url: 'https://github.com/dhananjayvandekar/All-task-and-doc.-of-AWS.git'
             }
         }
         stage ('build') {
