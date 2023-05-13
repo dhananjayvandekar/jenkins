e8413fde6f83ecbd911f87dae2138c1e297dfb64
dhanu <vandekardhananjay721@gmail.com>
2023-05-13 19:53:48 +0530

    ok

--- studentapp.groovy
+++ studentapp.groovy
@@ -16,7 +16,6 @@ pipeline {
                  sh '/opt/apache-maven/bin mvn sonar:sonar -Dsonar.projectKey=student.key -Dsonar.host.url=http://54.193.113.234:9000 -Dsonar.login=a0bb8570f57ed8843256f5b562fd4bd7f94388d8'
             }
         }
-        /*
         }
         
     }
