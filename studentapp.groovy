72bbea71bb5e1aff7558c188e1ded9dccf1ddf2b
dhanu <vandekardhananjay721@gmail.com>
2023-05-13 20:28:51 +0530

    qw

--- studentapp.groovy
+++ studentapp.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage ('pull') {
             steps {
-                git 'https://github.com/shubhamkalsait/studentapp-ui '
+                git 'https://github.com/shubhamkalsait/studentapp-ui'
             }
         }
         stage ('build') {
