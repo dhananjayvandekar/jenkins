1bbd4938516a3d171f4f74f282a4845fbae6e1f5
dhanu <vandekardhananjay721@gmail.com>
2023-06-11 12:27:45 +0530

    OK

--- studentapp.groovy
+++ studentapp.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages{
         stage ('pull') {
             steps {
-                 git 'https://github.com/shubhamkalsait/studentapp-ui'
+                 git 'https://github.com/dhananjayvandekar/All-task-and-doc.-of-AWS.git'
             }
         }
     }
