04525a8e2d03ed57f999ea04f4e0f8781cbe914c
dhanu <vandekardhananjay721@gmail.com>
2023-06-11 18:21:55 +0530

    ok

--- vishal.groovy
+++ vishal.groovy
@@ -14,20 +14,20 @@ pipeline {
         stage ('test') {
             steps {
                 withSonarQubeEnv(installationName: 'sonar-server', credentialsId: 'sonar-token') {
-                    sh ' /opt/apache-maven/bin/mvn sonar:sonar -Dsonar.projectKey=' 
+                    sh ' /opt/apache-maven/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp' 
                 }
             }
         }
-        // stage('quality-gate'){
-        //     steps {
-        //         timeout(time: 1, unit: 'HOURS') {
-        //             waitForQualityGate abortPipeline: true                    
-        //             }
-        //     }
-        // }
+        stage('quality-gate'){
+            steps {
+                timeout(time: 1, unit: 'HOURS') {
+                    waitForQualityGate abortPipeline: true                    
+                    }
+            }
+        }
         stage ('deploy') {
             steps {
-                deploy adapters: [tomcat8(credentialsId: 'tomcat-token', path: '', url: 'http://18.182.11.228:8080')], contextPath: '/', war: '**/*.war'
+                echo "deploy success"
             }
         }
     }
