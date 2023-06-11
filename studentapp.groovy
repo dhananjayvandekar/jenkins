af1f4fcb7d1c7d9eae4699114a36a84180d8cefd
dhanu <vandekardhananjay721@gmail.com>
2023-06-11 12:11:49 +0530

    ok

--- studentapp.groovy
+++ studentapp.groovy
@@ -1,25 +1,8 @@
 pipeline {
     agent any
-    stages {
-        stage('pull') {
-            steps {
-                git 'https://github.com/shubhamkalsait/studentapp-ui'
-            }
-        }
-        stage('build') {
-            steps {
-                sh '/opt/apache-maven/bin/mvn clean package'
-            }
-        }
-        stage ('test') {
-            steps {
-                echo 'test ok'
-            }
-        }
-        stage('pull') {
-            steps {
-                echo 'pull ok'
-            }
+    stages{
+        stage ('pull') {
+            git 'https://github.com/shubhamkalsait/studentapp-ui'
         }
     }
 }
