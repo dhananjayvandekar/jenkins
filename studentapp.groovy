2ec9bf456e7844f80d50f4b2c6ac4f7e094c29c1
dhanu <vandekardhananjay721@gmail.com>
2023-06-11 16:41:44 +0530

    ok

--- studentapp.groovy
+++ studentapp.groovy
@@ -1,33 +1,33 @@
 pipeline {
     agent any
-    stages{
+    stages {
         stage ('pull') {
-            steps {
-                 git 'https://github.com/shubhamkalsait/studentapp-ui.git'
+            steps{
+                git 'https://github.com/shubhamkalsait/studentapp-ui.git'
             }
         }
         stage ('build') {
             steps {
-                 sh '/opt/apache-maven/bin/mvn clean package'
+               sh  '/opt/apache-maven/bin/mvn clean package'
             }
         }
         stage ('test') {
             steps {
-              withSonarQubeEnv(installationName: 'sonar-server', credentialsId: 'sonar-token') {
+                withSonarQubeEnv(installationName: 'sonar-server', credentialsId: 'sonar-token') {
                     sh ' /opt/apache-maven/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp' 
-                }     
+                }
             }
         }
-        stage ('quality gate') {
-            steps { 
-                time out ( time: 1, unit:'HOURS') {
-                    waitforQualitygate abortPipeline: true
-                }
+        stage('quality-gate'){
+            steps {
+                timeout(time: 1, unit: 'HOURS') {
+                    waitForQualityGate abortPipeline: true                    
+                    }
             }
         }
         stage ('deploy') {
             steps {
-                echo "deply success"
+                echo "deploy success"
             }
         }
     }
